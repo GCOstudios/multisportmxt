0cb909b5d9f6f56674c515140dafed402a8f08ec
Jose Guerra <jose@gcostudios.com>
2018-07-16 22:55:56 +0100

    updated markup

--- content-blog.php
+++ content-blog.php
@@ -5,39 +5,42 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-			<?php if ( 'post' == get_post_type() ) : ?>
-		
-        	<div class="entry-meta custom_color">
-				<?php fitcoach_posted_on(); ?>
-			</div><!-- .entry-meta -->
-		
-			<?php endif; ?>
-        
+		<div class="featured-image-container">
 			<?php the_post_thumbnail('thumbnail', array('class' => 'fc-post-image')); ?>
-		</header><!-- .entry-header -->
+		</div>
+		<div class="content-container">
+			<header class="entry-header">
+				<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-		<div class="entry-content">
+				<?php if ( 'post' == get_post_type() ) : ?>
 			
-            <?php
-				if ( 'option2' == fitcoach_sanitize_index_content( get_theme_mod( 'fitcoach_post_content' ) ) ) :
-				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'fitcoach' ) );
-				else :
-				the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'fitcoach' ) );
-				endif; 
-			?> 
-            
-			<?php
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . __( 'Pages:', 'fitcoach' ),
-					'after'  => '</div>',
-				) );
-			?>
-		</div><!-- .entry-content -->
+				<div class="entry-meta custom_color">
+					<?php fitcoach_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			
+				<?php endif; ?>
+			</header><!-- .entry-header -->
 
-		<footer class="entry-footer">
-			<a href="<?php the_permalink(); ?>"><button><?php echo __( 'Read More', 'fitcoach' ); ?></button></a> 
-		</footer><!-- .entry-footer -->
+			<div class="entry-content">
+				
+				<?php
+					if ( 'option2' == fitcoach_sanitize_index_content( get_theme_mod( 'fitcoach_post_content' ) ) ) :
+					the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'fitcoach' ) );
+					else :
+					the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'fitcoach' ) );
+					endif; 
+				?> 
+				
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . __( 'Pages:', 'fitcoach' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<a href="<?php the_permalink(); ?>"><button><?php echo __( 'Read More', 'fitcoach' ); ?></button></a> 
+			</footer><!-- .entry-footer -->
+		</div>
 	</article><!-- #post-## --> 
